36464e9874b83e05b9ddb2a89d10d6d11ad95bca
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-14 15:09:04 +0200

    task 2 done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const slugify = string => {
-  const lower = string.toLowerCase();
-  const spliting = lower.split(' ');
-  return spliting.join('-');
+const filterArray = (numbers, value) => {
+  const newAr = [];
+  for (number of numbers) {
+    if (number > value) {
+      newAr.push(number);
+    }
+  }
+  return newAr;
 };
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
