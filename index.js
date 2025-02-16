9a3008c31b4219e29dfc5561aec327c608ae0add
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-16 15:22:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const book = {
-  title: 'The Last Kingdom',
-  author: 'Bernard Cornwell',
-  genres: ['historical prose', 'adventure'],
-  rating: 8.38,
-};
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
 
-const propertyB = Object.keys(book);
-
-for (const key of propertyB) {
-  console.log(propertyB);
-  console.log(book[key]);
+const hexColors = [];
+const rgbColors = [];
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
 }
+
+console.log(hexColors);
+console.log(rgbColors);
