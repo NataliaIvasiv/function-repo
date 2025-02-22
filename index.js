3785c393eea29b606d4419b74b2cdd1829db67e2
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-22 19:10:53 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
-const list = document.querySelector('.list');
+const singleBtn = document.querySelector('#single');
 
-const markup = technologies
-  .map(technology => `<li class="list-item">${technology}</li>`)
-  .join('');
+const handleClick = () => {
+  console.log('click event listener callback');
+};
 
-console.log(markup);
+singleBtn.addEventListener('click', handleClick);
 
-list.insertAdjacentHTML('beforeend', markup);
+// ===============================================
+const multiBtn = document.querySelector('#multiple');
+
+const firstCallback = () => {
+  console.log('First callback!');
+};
+const secondCallback = () => {
+  console.log('Second callback!');
+};
+const thirdCallback = () => {
+  console.log('Third callback!');
+};
+
+multiBtn.addEventListener('click', firstCallback);
+multiBtn.addEventListener('click', secondCallback);
+multiBtn.addEventListener('click', thirdCallback);
