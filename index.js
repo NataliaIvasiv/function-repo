d923feb724eb7c9a010532f4cdb6b9510314e0e7
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-22 15:26:16 +0200

    add markup

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-const article = document.querySelector('.article');
-console.log(article.innerHTML);
-
-const title = document.querySelector('.article .title');
-console.log(title.innerHTML);
-
-const text = document.querySelector('.article .text');
-console.log(text.innerHTML);
-
-const link = document.querySelector('.article .link');
-console.log(link.innerHTML);
+const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+const list = document.querySelector('.list');
