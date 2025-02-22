9257867e4d8a2cdda27f54e09960703a7b4d71bd
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-22 15:24:02 +0200

    innerHTML

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-class User {
-  constructor(name, email) {
-    console.log(name, email);
-  }
-}
+const article = document.querySelector('.article');
+console.log(article.innerHTML);
 
-const mango = new User('Mango', 'mango@mail.com'); // "Mango mango@mail.com"
+const title = document.querySelector('.article .title');
+console.log(title.innerHTML);
+
+const text = document.querySelector('.article .text');
+console.log(text.innerHTML);
+
+const link = document.querySelector('.article .link');
+console.log(link.innerHTML);
