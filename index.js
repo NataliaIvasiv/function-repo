7c7f23e98b05902cf1dda722b1760bf9cd48b5fa
Natalia <143248403+NataliaIvasiv@users.noreply.github.com>
2025-02-26 14:26:33 +0200

    form submit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const input = document.querySelector('[id="name-input"]');
-const output = document.querySelector('[id="name-output"]');
+// const input = document.querySelector('[id="name-input"]');
+// const output = document.querySelector('[id="name-output"]');
 
-input.addEventListener('input', e => {
-  let outputValue = e.currentTarget.value.trim();
-  if (!outputValue) {
-    return (output.textContent = 'Anonymous');
+// input.addEventListener('input', e => {
+//   let outputValue = e.currentTarget.value.trim();
+//   if (!outputValue) {
+//     return (output.textContent = 'Anonymous');
+//   } else {
+//     return (output.textContent = outputValue);
+//   }
+// });
+
+const form = document.querySelector('.login-form');
+//console.log(form);
+
+form.addEventListener('submit', submitHandler);
+function submitHandler(e) {
+  e.preventDefault();
+  const email = e.target.elements.email.value;
+  const password = e.target.elements.password.value;
+  let objectName = {};
+  if (email === '' || password === '') {
+    alert('fill in!');
   } else {
-    return (output.textContent = outputValue);
+    objectName.login = email;
+    objectName.keyWord = password;
+    console.log(objectName);
   }
-});
+  e.target.reset;
+}
